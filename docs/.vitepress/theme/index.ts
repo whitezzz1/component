fc1bdffb6b374f12d543eb89df5f0e35956c6506
zxf <1744017527@qq.com>
2024-10-30 16:34:04 +0800

    feat(custom): ii

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,15 @@
+import Theme from 'vitepress/theme'
+import SSYUI from '../../../src/entry'
+import 'vitepress-theme-demoblock/dist/theme/styles/index.css'
+// 插件的组件，主要是demo组件
+import Demo from 'vitepress-theme-demoblock/dist/client/components/Demo.vue'
+import DemoBlock from 'vitepress-theme-demoblock/dist/client/components/DemoBlock.vue'
+
+export default {
+  ...Theme,
+  enhanceApp({ app }) {
+    app.use(SSYUI)
+    app.component('Demo', Demo)
+    app.component('DemoBlock', DemoBlock)
+  },
+}
---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import UnoCSS from 'unocss/vite'
+/// <reference types="vitest/config" />
+import { defineConfig } from 'vite'
+
+const rollupOptions = {
+  external: ['vue', 'vue-router'],
+  output: {
+    globals: {
+      vue: 'Vue',
+    },
+  },
+}
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    UnoCSS(),
+  ],
+  build: {
+    rollupOptions,
+    minify: false,
+    cssCodeSplit: true,
+    sourcemap: true,
+    lib: {
+      entry: './src/entry.ts',
+      name: 'SSYUI',
+      fileName: 'ssy-ui',
+      formats: ['es', 'umd', 'iife'],
+    },
+  },
+  test: {
+    // enable jest-like global test APIs
+    globals: true,
+    // simulate DOM with happy-dom
+    // (requires installing happy-dom as a peer dependency)
+    environment: 'happy-dom',
+  },
+})
